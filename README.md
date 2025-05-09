ba8d30d37f0385daa72f11545ed1d3fb7020910b
Gabriel Trevisan Ribeiro <162700611+gTrevisanz@users.noreply.github.com>
2025-05-09 18:52:27 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# React-petshop
+# PetshopReact
+
+## Como Rodar
+
+-   Depois de clonado, no terminal rodar: `cd PetshopReact`
+-   Depois, rodar: `npm i`
+-   Depois que intalou as dependências, rodar: `npm start`
--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import BanhoTosa from './pages/BanhoTosa'
-import HotelResort from './pages/HotelResort';
-import Adestramento from './pages/Adestramento';
-import NavbarMenu from './pages/NavbarMenu';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BanhoTosa from "./pages/BanhoTosa";
+import HotelResort from "./pages/HotelResort";
+import Adestramento from "./pages/Adestramento";
+import NavbarMenu from "./components/NavbarMenu";
+import Banner from "./components/Banner"
 
-function App() {
-  return (
-    <Router>
+const App = () => {
+	return (
+		<Router>
+			<header>
+				<NavbarMenu />
+			</header>
 
-      <NavbarMenu />
-      
-      <Routes>
-          <Route path="/" element={ <Home />} />
-          <Route path="/banho-e-tosa"  element={ <BanhoTosa />} />
-          <Route path="/hotel-resort"  element={ <HotelResort />} />
-          <Route path="/adestramento"  element={ <Adestramento />} />
-      </Routes>
-    </Router>
-  );
-}
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/banho-e-tosa" element={<BanhoTosa />} />
+				<Route path="/hotel-resort" element={<HotelResort />} />
+				<Route path="/adestramento" element={<Adestramento />} />
+			</Routes>
+			<Banner/>
+		</Router>
+		
+	);
+};
 
 export default App;
